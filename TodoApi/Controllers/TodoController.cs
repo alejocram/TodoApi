429c576c835344fbd4440954baa34a38c1574fb5
Jose Alejandro Carmona Ramirez <jcarmon4@eafit.edu.co>
2018-03-21 18:27:23 -0500

    Implementating Create, Update and Delete

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -45,22 +45,58 @@ namespace TodoApi.Controllers
             return new ObjectResult(item);
         }
 
-        //// POST api/<controller>
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Create([FromBody]TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
 
-        //// PUT api/<controller>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
 
-        //// DELETE api/<controller>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if(todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+        }
     }
 }
